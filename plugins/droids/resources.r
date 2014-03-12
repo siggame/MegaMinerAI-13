1ac24fc8dd261108af4b94b170355463b15e427e
Wyatt Kennedy <wykennedy@gmail.com>
2014-03-12 15:45:11 -0500

    test shader added

--- plugins/droids/resources.r
+++ plugins/droids/resources.r
@@ -1,3 +1,4 @@
 font DroidSansMono textures/fonts/DroidSansMono.png DroidSansMono.dat
 font Roboto textures/fonts/Roboto.png Roboto.dat
 texture texture textures/texture.png
+shader color shader/color.megashader
