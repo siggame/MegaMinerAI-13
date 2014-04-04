2b313fdddd520006f14666f03f3b634d7912f486
Russley Shaw <rfswf5@mst.edu>
2014-04-04 18:13:47 -0500

    Added Cache

--- testClients/ruski/AI.py
+++ testClients/ruski/AI.py
@@ -2,9 +2,15 @@
 from BaseAI import BaseAI
 from GameObject import *
 
+import random
+
 import game_history
+import game_cache
 
 class AI(BaseAI):
+
+  self.manhattan_offsets = [(0,1),(0,-1),(1,0),(-1,0)]
+
   """The class implementing gameplay logic."""
   @staticmethod
   def username():
@@ -17,6 +23,7 @@ class AI(BaseAI):
   ##This function is called once, before your first turn
   def init(self):
     self.history = game_history.game_history(self, True)
+    self.cache = game_cache.game_cache(self)
     pass
 
   ##This function is called once, after your last turn
@@ -29,6 +36,22 @@ class AI(BaseAI):
   def run(self):
     snapshot = self.history.save_snapshot()
     self.history.print_snapshot(snapshot)
+    self.game_cache.update_all()
+
+    #DROP SOMETHING
+    randx = random.randrange(0, self.mapWidth-1)
+    randy = random.randrange(0, self.mapHeight-1)
+    randv = random.randrange(0, 7)
+    self.players[self.playerID].orbitalDrop(randx, randy, randv)
+
+    for droid in self.droids:
+      if droid.owner == self.playerID:
+        offset = random.choice(self.manhattan_offsets)
+        droid.move(offset[0], offset[1])
+
+        offset = random.choice(self.manhattan_offsets)
+        droid.operate(offset[0], offset[1])
+
     
     return 1
 
---
+++ testClients/ruski/game_cache.py
@@ -0,0 +1,64 @@
+class game_cache:
+
+  def __init__(self, ai):
+    self.ai = ai
+
+    self.my_spawning = dict()
+    self.enemy_spawning = dict()
+
+    self.my_hangars = dict()
+    self.enemy_hangars = dict()
+
+    self.walls = dict()
+
+    self.my_droids = dict()
+    self.enemy_droids = dict()
+
+  def update_all(self):
+    self.update_tiles()
+    self.update_droids()
+
+  def update_droids(self):
+    self.my_hangars = dict()
+    self.enemy_hangars = dict()
+
+    self.walls = dict()
+
+    self.my_droids = dict()
+    self.enemy_droids = dict()
+
+
+    for droid in self.ai.droids:
+      if droid.variant == 5: #WALL
+        self.walls[(droid.x, droid.y)] = droid
+
+      elif droid.variant == 7: #HANGAR
+        if droid.owner == self.playerID:
+          self.my_hangars[(droid.x, droid.y)] = droid
+        elif droid.owner == self.playerID^1:
+          self.enemy_hangars[(droid.x, droid.y)] = droid
+
+      else: #DROID
+        if droid.owner == self.playerID:
+          self.my_droids[(droid.x, droid.y)] = droid
+        elif droid.owner == self.playerID^1:
+          self.enemy_droids[(droid.x, droid.y)] = droid
+
+    return True
+
+
+  def update_tiles(self):
+    self.my_spawning = dict()
+    self.enemy_spawning = dict()
+
+    for tile in self.ai.tiles:
+      if tile.turnsUntilAssembled > 0: #ASSEMBLING TILE
+        if tile.owner == self.ai.playerID:
+          self.my_spawning[(tile.x, tile.y)] = tile
+        elif tile.owner == self.ai.playerID^1:
+          self.enemy_droids[(tile.x, tile.y)] = tile
+    return True
+
+
+
+
