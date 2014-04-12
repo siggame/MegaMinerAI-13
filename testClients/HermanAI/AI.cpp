c94d48d4be60db677cd5db52e61739d973406c4a
jjherman94 <jjhf39@mst.edu>
2014-04-12 00:37:24 -0500

    improved movement

--- testClients/HermanAI/AI.cpp
+++ testClients/HermanAI/AI.cpp
@@ -76,6 +76,50 @@ void AI::spawnDemDroids()
   }
 }
 
+void AI::moveTo(Droid & droid, int x, int y)
+{
+  for(int i=0; i<droid.maxMovement(); i++)
+  {
+    bool moved=false;
+    if(droid.x()<x && validMove(droid.x()+1,droid.y()))
+    {
+      moved=true;
+      droid.move(droid.x()+1,droid.y());
+    }
+    else if(droid.x()>x && validMove(droid.x()-1,droid.y()))
+    {
+      moved=true;
+      droid.move(droid.x()-1,droid.y());
+    }
+    else if(droid.y()<y && validMove(droid.x(),droid.y()+1))
+    {
+      moved=true;
+      droid.move(droid.x(),droid.y()+1);
+    }
+    else if(droid.y()>y && validMove(droid.x(),droid.y()-1))
+    {
+      moved=true;
+      droid.move(droid.x(),droid.y()-1);
+    }
+    if(!moved)
+    {
+      int temp;
+      if(rand()%2==0)
+        temp=-1;
+      else
+        temp=1;
+      if(droid.x()<x && !validMove(droid.x()+1,droid.y()))
+        droid.move(droid.x(),droid.y()+temp);
+      else if(droid.x()>x && !validMove(droid.x()-1,droid.y()))
+        droid.move(droid.x(),droid.y()+temp);
+      else if(droid.y()<y && !validMove(droid.x(),droid.y()+1))
+        droid.move(droid.x()+temp,droid.y());
+      else if(droid.y()>y && !validMove(droid.x(),droid.y()-1))
+        droid.move(droid.x()+temp,droid.y());
+    }
+  }
+}
+
 void AI::doStuffs()
 {
   for(int i = 0; i < droids.size(); i++)
@@ -85,36 +129,8 @@ void AI::doStuffs()
        (droids[i].owner() != playerID() && droids[i].hackedTurnsLeft() > 0))
     {
       //if there are any moves to be done
-      if(droids[i].movementLeft() > 0)
-      {
-        //try to move towards the enemy
-        int changeX = 1;
-        //if on the right move towards the left
-        if(playerID() == 1)
-        {
-          changeX = -1;
-        }
-        bool move = true;
-        //check if there is a droid on that tile
-        for(int z = 0; z < droids.size(); z++)
-        {
-          //if the two droids are different
-          if(droids[z].id() != droids[i].id())
-          {
-            //if there is a droid to run into
-            if(droids[z].x() == droids[i].x() + changeX && droids[z].y() == droids[i].y())
-            {
-              //don't move
-              move = false;
-            }
-          }
-        }
-        //move if okay and within map boundaries
-        if(move && droids[i].x() + changeX >= 0 && droids[i].x() + changeX < mapWidth())
-        {
-          droids[i].move(droids[i].x() + changeX, droids[i].y());
-        }
-      }
+      Droid* droid = getNearestHangar(droids[i].x(), droids[i].y());
+      moveTo(droids[i], droid->x(), droid->y());
      
       //if there are any attacks left
       if(droids[i].attacksLeft() > 0)
@@ -220,6 +236,7 @@ Droid* AI::getEnemyInRange(int xloc, int yloc, int range)
   }
   return NULL;
 }
+
 Droid* AI::getFriendInRange(int xloc, int yloc, int range)
 {
   for(int i=0; i< friendDroids.size(); i++)
@@ -230,3 +247,41 @@ Droid* AI::getFriendInRange(int xloc, int yloc, int range)
   }
   return NULL;
 }
+
+Droid* AI::getNearestHangar(int xloc, int yloc)
+{
+  int minDist=10000;
+  int xMin, yMin;
+  int distance;
+  int hangarNum;
+  for(int i=0; i<enemyHangars.size(); i++)
+  {
+    distance = abs(enemyHangars[i]->x()-xloc) + abs(enemyHangars[i]->y()-yloc);
+    if(distance<minDist)
+    {
+      minDist=distance;
+      xMin=enemyHangars[i]->x();
+      yMin=enemyHangars[i]->y();
+      hangarNum=i;
+    }
+  }
+  return enemyHangars[hangarNum];
+}
+
+bool AI::validMove(const int x, const int y)
+{
+  for(int i=0; i< droids.size(); i++)
+  {
+    if(droids[i].x()==x && droids[i].y()==y)
+      return false;
+  }
+  Tile* tile =getTile(x, y);
+  if(tile-> turnsUntilAssembled() == 1)
+  {
+    return false;
+  }
+  return true;
+}
+
+
+
