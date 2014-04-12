665d829ff75e144a77d8fc4df2d32bae9a426115
Adam Harter <atbh79@mail.mst.edu>
2014-04-12 17:59:08 -0500

    Fixed up AI, doesn't produce 1000's of errors anymore.

--- testClients/adamTest/AI.py
+++ testClients/adamTest/AI.py
@@ -7,7 +7,7 @@ class AI(BaseAI):
   """The class implementing gameplay logic."""
   @staticmethod
   def username():
-    return "Pell, May I?"
+    return unicode("Pell, May I?")
 
   @staticmethod
   def password():
@@ -15,45 +15,7 @@ class AI(BaseAI):
 
   ##This function is called once, before your first turn
   def init(self):
-    self.enemyMinX = 524
-    self.enemyMinY = 234
-    self.enemyMaxX = 0
-    self.enemyMaxY = 0
-    self.minX = 300
-    self.minY = 300
-    self.maxX = 0
-    self.maxY = 0
     self.change = -((self.playerID * 2) - 1)
-    for droid in self.droids:
-      if droid.owner == self.playerID:
-        if self.maxX < droid.x:
-          self.maxX = droid.x
-        if self.maxY < droid.y:
-          self.maxY = droid.y
-        if self.minX > droid.x:
-          self.minX = droid.x
-        if self.minY > droid.y:
-          self.minY = droid.y
-      else:
-        if self.enemyMaxX < droid.x:
-          self.enemyMaxX = droid.x
-        if self.enemyMaxY < droid.y:
-          self.enemyMaxY = droid.y
-        if self.enemyMinX > droid.x:
-          self.enemyMinX = droid.x
-        if self.enemyMinY > droid.y:
-          self.enemyMinY = droid.y
-    self.minY -= 1
-    self.maxY += 1
-    if self.playerID == 0:
-      self.dropX = self.maxX + 1
-      self.dropY = self.minY
-      self.startX = self.maxX
-    else:
-      self.dropX = self.minX - 1
-      self.dropY = self.minY
-      self.startX = self.minX
-
     pass
 
   ##This function is called once, after your last turn
@@ -67,17 +29,7 @@ class AI(BaseAI):
   randVars = [0,1,2,3,6]
 
   def run(self):
-
-    #if self.dropY == self.maxY or self.dropY == self.minY:
-    #  self.players[self.playerID].orbitalDrop((self.mapWidth - 1)*self.playerID, self.dropY, self.randVars[random.randint(0,3)])
-    #else:
-    #  self.players[self.playerID].orbitalDrop(self.dropX, self.dropY, self.randVars[random.randint(0,3)])
-    #self.dropY += 1
-    #if self.dropY > self.maxY:
-    #  self.dropY = self.minY
-
     meh = self.randVars[random.randint(0,len(self.randVars) - 1)]
-
     if self.players[self.playerID].scrapAmount > 80:
       bleh = 0
       while bleh < 10:
@@ -99,7 +51,7 @@ class AI(BaseAI):
         bleh = []
         if droid.variant == 3:
           for droid2 in self.droids:
-            if droid2.owner != self.playerID:
+            if droid2.owner != self.playerID and droid2.variant != 7 and droid2.variant != 5:
               if abs(droid2.x - droid.x) + abs(droid2.y - droid.y) <= droid.range + droid.maxMovement:
                 bleh.append(droid2)
         elif droid.attack > 0:
@@ -109,7 +61,7 @@ class AI(BaseAI):
                 bleh.append(droid2)
         else:
           for droid2 in self.droids:
-            if droid2.owner == self.playerID:
+            if droid2.owner == self.playerID and droid2.maxArmor != droid2.armor:
               if abs(droid2.x - droid.x) + abs(droid2.y - droid.y) <= droid.range + droid.maxMovement:
                 bleh.append(droid2)
         #attack even if a turret
@@ -126,7 +78,7 @@ class AI(BaseAI):
                 self.distance = (abs(target.x - droid.x) + abs(target.y - droid.y))
                 target2 = target
           elif droid.attack < 0:
-            if target.variant != 7 and target.owner == self.playerID and target.id != droid.id:
+            if target.variant != 7 and target.owner == self.playerID and target.id != droid.id and droid2.maxArmor != droid2.armor:
               if (abs(target.x - droid.x) + abs(target.y - droid.y)) < self.distance:
                 self.distance = (abs(target.x - droid.x) + abs(target.y - droid.y))
                 target2 = target
@@ -159,23 +111,40 @@ class AI(BaseAI):
           if move and droid.movementLeft > 0:
             if target is not None:
               if target.x > droid.x:
-                if not droid.move(droid.x + 1, droid.y ):
-                  if not droid.move(droid.x, droid.y - 1):
-                    droid.move(droid.x, droid.y + 1)
+                if self.meh(droid.x + 1, droid.y) is None and droid.x != self.mapWidth - 1:
+                  droid.move(droid.x + 1, droid.y)
+                elif self.meh(droid.x, droid.y - 1) is None and droid.y != 0:
+                  droid.move(droid.x, droid.y - 1)
+                elif self.meh(droid.x, droid.y + 1) is None and droid.y != self.mapHeight - 1:
+                  droid.move(droid.x, droid.y + 1)
               elif target.x < droid.x:
-                if not droid.move(droid.x - 1, droid.y ):
-                  if not droid.move(droid.x, droid.y - 1):
-                    droid.move(droid.x, droid.y + 1)
+                if self.meh(droid.x - 1, droid.y) is None and droid.x != 0:
+                  droid.move(droid.x - 1, droid.y)
+                elif self.meh(droid.x, droid.y - 1) is None and droid.y != 0:
+                  droid.move(droid.x, droid.y - 1)
+                elif self.meh(droid.x, droid.y + 1) is None and droid.y != self.mapHeight - 1:
+                  droid.move(droid.x, droid.y + 1)
               elif target.y > droid.y:
-                if not droid.move(droid.x , droid.y + 1):
-                  if not droid.move(droid.x + self.change, droid.y):
-                    droid.move(droid.x - self.change, droid.y)
+                if self.meh(droid.x, droid.y + 1) is None and droid.y != self.mapHeight - 1:
+                  droid.move(droid.x, droid.y + 1)
+                elif self.meh(droid.x - 1, droid.y) is None and droid.x != 0:
+                  droid.move(droid.x - 1, droid.y)
+                elif self.meh(droid.x + 1, droid.y) is None and droid.x != self.mapWidth - 1:
+                  droid.move(droid.x + 1, droid.y )
               elif target.y < droid.y:
-                if not droid.move(droid.x, droid.y - 1):
-                  if not droid.move(droid.x + self.change, droid.y):
-                    droid.move(droid.x - self.change, droid.y)
-
+                if self.meh(droid.x, droid.y - 1) is None and droid.y != 0:
+                  droid.move(droid.x, droid.y - 1)
+                elif self.meh(droid.x - 1, droid.y) is None and droid.x != 0:
+                  droid.move(droid.x - 1, droid.y)
+                elif self.meh(droid.x + 1, droid.y) is None and droid.x != self.mapWidth - 1:
+                  droid.move(droid.x + 1, droid.y )
     return 1
+
+  def meh(self, x, y):
+    for droid in self.droids:
+      if x == droid.x and y == droid.y:
+        return droid
+    return None
 
   def __init__(self, conn):
     BaseAI.__init__(self, conn)
